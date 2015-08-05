6be76d6d042ca81720f8a8694510f33f0d956c73
Sybren A. Stüvel <sybren@stuvel.eu>
2015-08-05 10:29:20 +0200

    Added MATERIAL_OT_link_to_base_names
    It checks all objects in the scene, and re-links materials to their
    base name, so a slot currently using "Material.001" becomes linked
    to "Material" etc.
    
    Made for TynkaTopi in #blender

---
+++ material_ot_link_to_base_names.py
@@ -0,0 +1,73 @@
+bl_info = {
+    'name': 'Material fixer',
+    'author': 'Sybren A. Stüvel',
+    'version': (0, 1),
+    'blender': (2, 75, 0),
+    'location': 'Press [Space], search for "link materials"',
+    'category': 'Material',
+}
+
+
+import bpy
+
+
+class MATERIAL_OT_link_to_base_names(bpy.types.Operator):
+    bl_idname = "material.link_to_base_names"
+    bl_label = "Link materials to base names"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        for ob in context.scene.objects:
+            for slot in ob.material_slots:
+                self.fixup_slot(slot)
+            for idx, mat in enumerate(ob.data.materials):
+                self.fixup_mat(idx, mat)
+                
+        return {'FINISHED'}
+    
+    def split_name(self, material):
+        name = material.name
+
+        if not '.' in name:
+            return name, None
+        
+        base, suffix = name.rsplit('.', 1)
+        try:
+            num = int(suffix, 10)
+        except ValueError:
+            # Not a numeric suffix
+            return name, None
+        
+        return base, suffix
+    
+    def fixup_slot(self, slot):
+        if not slot.material:
+            return
+    
+        base, suffix = self.split_name(slot.material)
+        if suffix is None:
+            return
+        
+        try:
+            base_mat = bpy.data.materials[base]
+            print('Base material %r not found' % base)
+        except KeyError:
+            return
+        
+        slot.material = base_mat             
+        
+    
+    def fixup_mat(self, idx, mat):
+        pass
+
+
+def register():
+    bpy.utils.register_class(MATERIAL_OT_link_to_base_names)
+
+
+def unregister():
+    bpy.utils.unregister_class(MATERIAL_OT_link_to_base_names)
+
+
+if __name__ == "__main__":
+    register()
