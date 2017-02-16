4a4cc451178890318353f52e343d60baff45cf07
Sybren A. Stüvel <sybren@stuvel.eu>
2017-02-16 11:04:19 +0100

    Selection set selector: only select (don't hide) and use Shift+Alt+W

--- selection_set_selector.py
+++ selection_set_selector.py
@@ -16,9 +16,9 @@ from bpy.types import Operator, Menu
 from bpy.props import StringProperty
 
 
-class POSE_OT_show_single_selection_set(Operator):
-    bl_idname = 'post.show_single_selection_set'
-    bl_label = 'Show Single Selection Set'
+class POSE_OT_select_selection_set(Operator):
+    bl_idname = 'post.select_selection_set'
+    bl_label = 'Select Selection Set'
     bl_options = {'UNDO', 'REGISTER'}
 
     selection_set_name = StringProperty(name='Selection Set Name')
@@ -35,19 +35,15 @@ class POSE_OT_show_single_selection_set(Operator):
         arm = context.object
         sel_set = arm.selection_sets[self.selection_set_name]
 
-        # Only show bones in the selection set
-        for pose_bone in arm.pose.bones:
-            pose_bone.bone.hide = pose_bone.name not in sel_set.bone_ids
-
-            # Ensure that hidden bones are no longer selected.
-            if pose_bone.bone.hide:
-                pose_bone.bone.select = False
+        for bone in context.visible_pose_bones:
+            if bone.name in sel_set.bone_ids:
+                 bone.bone.select = True
 
         return {'FINISHED'}
 
 
-class POSE_OT_show_single_selection_set_call_menu(Operator):
-    bl_idname = 'pose.show_single_selection_set_call_menu'
+class POSE_OT_select_selection_set_call_menu(Operator):
+    bl_idname = 'pose.select_selection_set_call_menu'
     bl_label = 'Show Single Selection Set'
 
     def execute(self, context):
@@ -56,14 +52,14 @@ class POSE_OT_show_single_selection_set_call_menu(Operator):
 
 
 class POSE_MT_selection_sets(Menu):
-    bl_label = POSE_OT_show_single_selection_set.bl_label
-    poll = POSE_OT_show_single_selection_set.poll
+    bl_label = POSE_OT_select_selection_set.bl_label
+    poll = POSE_OT_select_selection_set.poll
 
     def draw(self, context):
         layout = self.layout
         layout.operator_context = 'EXEC_DEFAULT'
         for ss in context.object.selection_sets:
-            props = layout.operator(POSE_OT_show_single_selection_set.bl_idname, text=ss.name)
+            props = layout.operator(POSE_OT_select_selection_set.bl_idname, text=ss.name)
             props.selection_set_name = ss.name
 
 
@@ -72,22 +68,22 @@ def add_ssss_button(self, context):
 
 
 def register():
-    bpy.utils.register_class(POSE_OT_show_single_selection_set)
-    bpy.utils.register_class(POSE_OT_show_single_selection_set_call_menu)
+    bpy.utils.register_class(POSE_OT_select_selection_set)
+    bpy.utils.register_class(POSE_OT_select_selection_set_call_menu)
     bpy.utils.register_class(POSE_MT_selection_sets)
 
     wm = bpy.context.window_manager
     km = wm.keyconfigs.active.keymaps['Pose']
 
-    kmi = km.keymap_items.new('wm.call_menu', 'W', 'PRESS', alt=True, ctrl=True)
+    kmi = km.keymap_items.new('wm.call_menu', 'W', 'PRESS', alt=True, shift=True)
     kmi.properties.name = 'POSE_MT_selection_sets'
 
     bpy.types.VIEW3D_MT_pose_showhide.append(add_ssss_button)
 
 
 def unregister():
-    bpy.utils.unregister_class(POSE_OT_show_single_selection_set)
-    bpy.utils.unregister_class(POSE_OT_show_single_selection_set_call_menu)
+    bpy.utils.unregister_class(POSE_OT_select_selection_set)
+    bpy.utils.unregister_class(POSE_OT_select_selection_set_call_menu)
     bpy.utils.unregister_class(POSE_MT_selection_sets)
     bpy.types.VIEW3D_MT_pose_showhide.remove(add_ssss_button)
 
