3908c61434cb9d8b8df0d34cb6595176e0dfe5dc
Sybren A. Stüvel <sybren@stuvel.eu>
2017-06-28 10:21:19 +0200

    rename_meta_to_content: added operator to (un)mute audio inside meta strip

--- rename_meta_to_content.py
+++ rename_meta_to_content.py
@@ -46,16 +46,54 @@ class SEQUENCER_OT_setup_meta(bpy.types.Operator):
         return {'FINISHED'}
 
 
+class SEQUENCER_OT_mute_audio(bpy.types.Operator):
+    bl_idname = 'sequencer.mute_audio'
+    bl_label = 'Toggle audio in meta strips'
+    bl_description = 'Mutes/unmutes the audio inside selected meta strips'
+    bl_options = {'REGISTER', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+        if not context.scene.sequence_editor:
+            return False
+        return any(s.type == 'META' for s in context.selected_sequences)
+
+    def execute(self, context):
+        # Figure out what to do (mute/unmute)
+        se = context.scene.sequence_editor
+        act_strip = se.active_strip
+        mute = True
+        for sub in act_strip.sequences:
+            if sub.type != 'SOUND':
+                continue
+            mute = not sub.mute
+            break
+
+        for strip in context.selected_sequences:
+            if strip.type != 'META':
+                continue
+
+            for sub in strip.sequences:
+                if sub.type != 'SOUND':
+                    continue
+                sub.mute = mute
+
+        return {'FINISHED'}
+
+
 def render_header(self, context):
         layout = self.layout
         layout.operator(SEQUENCER_OT_setup_meta.bl_idname)
+        layout.operator(SEQUENCER_OT_mute_audio.bl_idname)
 
 
 def register():
     bpy.utils.register_class(SEQUENCER_OT_setup_meta)
+    bpy.utils.register_class(SEQUENCER_OT_mute_audio)
     bpy.types.SEQUENCER_HT_header.append(render_header)
 
 
 def unregister():
     bpy.utils.unregister_class(SEQUENCER_OT_setup_meta)
+    bpy.utils.unregister_class(SEQUENCER_OT_mute_audio)
     bpy.types.SEQUENCER_HT_header.remove(render_header)
