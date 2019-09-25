8f479250dcbedd367367b4b4f6a5531a6e7718d3
Sybren A. Stüvel <sybren@stuvel.eu>
2019-09-25 15:51:02 +0200

    Copy Visual Transform: support auto-keying
    Doesn't support keying sets yet.

--- copy_visual_transform.py
+++ copy_visual_transform.py
@@ -14,7 +14,117 @@ bl_info = {
     'support': 'COMMUNITY',
 }
 
+from typing import Iterable, Optional, Set, Union
+
 import bpy
+
+
+class AutoKeying:
+    @classmethod
+    def keying_options(cls, context) -> Set[str]:
+        """Retrieve the general keyframing options from user preferences."""
+
+        prefs = context.preferences
+        ts = context.scene.tool_settings
+        options = set()
+
+        if prefs.edit.use_visual_keying:
+            options.add('INSERTKEY_VISUAL')
+        if prefs.edit.use_keyframe_insert_needed:
+            options.add('INSERTKEY_NEEDED')
+        if prefs.edit.use_insertkey_xyz_to_rgb:
+            options.add('INSERTKEY_XYZ_TO_RGB')
+        if ts.use_keyframe_cycle_aware:
+            options.add('INSERTKEY_CYCLE_AWARE')
+        return options
+
+    @classmethod
+    def autokeying_options(cls, context) -> Optional[Set[str]]:
+        """Retrieve the Auto Keyframe options, or None if disabled."""
+
+        ts = context.scene.tool_settings
+
+        if not ts.use_keyframe_insert_auto:
+            return None
+
+        if ts.use_keyframe_insert_keyingset:
+            # No support for keying sets (yet).
+            return None
+
+        prefs = context.preferences
+        options = cls.keying_options(context)
+
+        if prefs.edit.use_keyframe_insert_available:
+            options.add('INSERTKEY_AVAILABLE')
+        if ts.auto_keying_mode == 'REPLACE_KEYS':
+            options.add('INSERTKEY_REPLACE')
+        return options
+
+    @staticmethod
+    def get_4d_rotlock(bone: bpy.types.PoseBone) -> Iterable[bool]:
+        "Retrieve the lock status for 4D rotation."
+        if bone.lock_rotations_4d:
+            return [bone.lock_rotation_w, *bone.lock_rotation]
+        else:
+            return [all(bone.lock_rotation)] * 4
+
+    @staticmethod
+    def keyframe_channels(
+            target: Union[bpy.types.Object, bpy.types.PoseBone],
+            options: Set[str],
+            data_path: str,
+            group: str,
+            locks: Iterable[bool],
+        ) -> None:
+        if all(locks):
+            return
+
+        if not any(locks):
+            target.keyframe_insert(data_path, group=group, options=options)
+            return
+
+        for index, lock in enumerate(locks):
+            if lock:
+                continue
+            target.keyframe_insert(data_path, index=index, group=group, options=options)
+
+    @classmethod
+    def key_transformation(
+            cls,
+            target: Union[bpy.types.Object, bpy.types.PoseBone],
+            options: Set[str],
+        ) -> None:
+        """Keyframe transformation properties, avoiding keying locked channels."""
+
+        is_bone = isinstance(target, bpy.types.PoseBone)
+        if is_bone:
+            group = target.name
+        else:
+            group = 'Object Transforms'
+
+        def keyframe(data_path, locks):
+            cls.keyframe_channels(target, options, data_path, group, locks)
+
+        if not (is_bone and target.bone.use_connect):
+            keyframe('location', target.lock_location)
+
+        if target.rotation_mode == 'QUATERNION':
+            keyframe('rotation_quaternion', cls.get_4d_rotlock(target))
+        elif target.rotation_mode == 'AXIS_ANGLE':
+            keyframe('rotation_axis_angle', cls.get_4d_rotlock(target))
+        else:
+            keyframe('rotation_euler', target.lock_rotation)
+
+        keyframe('scale', target.lock_scale)
+
+    @classmethod
+    def autokey_transformation(cls, context, target: Union[bpy.types.Object, bpy.types.PoseBone]) -> None:
+        """Auto-key transformation properties."""
+
+        options = cls.autokeying_options(context)
+        if options is None:
+            return
+        cls.key_transformation(target, options)
 
 
 def get_matrix(context):
@@ -35,8 +145,10 @@ def set_matrix(context, mat):
         # Convert matrix to local space
         arm = context.active_object
         bone.matrix = arm.matrix_world.inverted() @ mat
+        AutoKeying.autokey_transformation(context, bone)
     else:
         context.active_object.matrix_world = mat
+        AutoKeying.autokey_transformation(context, context.active_object)
 
 
 class OBJECT_OT_copy_matrix(bpy.types.Operator):
@@ -46,10 +158,10 @@ class OBJECT_OT_copy_matrix(bpy.types.Operator):
                      'to the clipboard. Uses world-space matrices'
 
     @classmethod
-    def poll(cls, context):
+    def poll(cls, context) -> bool:
         return bool(context.active_pose_bone) or bool(context.active_object)
 
-    def execute(self, context):
+    def execute(self, context) -> Set[str]:
         context.window_manager.clipboard = repr(get_matrix(context))
         return {'FINISHED'}
 
@@ -61,10 +173,10 @@ class OBJECT_OT_paste_matrix(bpy.types.Operator):
                      'or object. Uses world-space matrices'
 
     @classmethod
-    def poll(cls, context):
+    def poll(cls, context) -> bool:
         return bool(context.active_pose_bone) or bool(context.active_object)
 
-    def execute(self, context):
+    def execute(self, context) -> Set[str]:
         from mathutils import Matrix
 
         mat = eval(context.window_manager.clipboard, {}, {'Matrix': Matrix})
@@ -79,7 +191,7 @@ class VIEW3D_PT_copy_matrix(bpy.types.Panel):
     bl_category = "View"
     bl_label = "Copy Matrix"
 
-    def draw(self, context):
+    def draw(self, context) -> None:
         layout = self.layout
 
         col = layout.column(align=True)
