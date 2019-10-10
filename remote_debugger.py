75df04a99206521c507e744f931c368c38bd7e7a
Sybren A. Stüvel <sybren@stuvel.eu>
2019-10-10 17:29:00 +0200

    Remote Debugger: remove console warnings when loading in Blender 2.80+

--- remote_debugger.py
+++ remote_debugger.py
@@ -27,6 +27,54 @@ from bpy.types import AddonPreferences
 from bpy.props import StringProperty
 
 
+# Get references to all property definition functions in bpy.props,
+# so that they can be used to replace 'x = IntProperty()' to 'x: IntProperty()'
+# dynamically when working on Blender 2.80+
+__all_prop_funcs = {
+    getattr(bpy.props, propname)
+    for propname in dir(bpy.props)
+    if propname.endswith('Property')
+}
+
+def convert_properties(class_):
+    """Class decorator to avoid warnings in Blender 2.80+
+
+    This decorator replaces property definitions like this:
+
+        someprop = bpy.props.IntProperty()
+
+    to annotations, as introduced in Blender 2.80:
+
+        someprop: bpy.props.IntProperty()
+
+    No-op if running on Blender 2.79 or older.
+    """
+
+    if bpy.app.version < (2, 80):
+        return class_
+
+    if not hasattr(class_, '__annotations__'):
+        class_.__annotations__ = {}
+
+    attrs_to_delete = []
+    for name, value in class_.__dict__.items():
+        if not isinstance(value, tuple) or len(value) != 2:
+            continue
+
+        prop_func, kwargs = value
+        if prop_func not in __all_prop_funcs:
+            continue
+
+        # This is a property definition, replace it with annotation.
+        attrs_to_delete.append(name)
+        class_.__annotations__[name] = value
+
+    for attr_name in attrs_to_delete:
+        delattr(class_, attr_name)
+
+    return class_
+
+
 def addon_preferences(context):
     try:
         preferences = context.preferences
@@ -37,6 +85,7 @@ def addon_preferences(context):
     return preferences.addons[__name__].preferences
 
 
+@convert_properties
 class DebuggerAddonPreferences(AddonPreferences):
     # this must match the addon name, use '__package__'
     # when defining this in a submodule of a python package.
