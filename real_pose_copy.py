4c76be46c4abf9e1c353587b728726c64f81446c
Sybren A. Stüvel <sybren@stuvel.eu>
2020-11-17 15:55:25 +0100

    Reformat with Black
    No functional changes.

--- real_pose_copy.py
+++ real_pose_copy.py
@@ -8,12 +8,12 @@ by name.
 """
 
 bl_info = {
-    'name': 'Real pose copy',
-    'author': 'Sybren A. Stüvel',
-    'version': (1, 1),
-    'blender': (2, 80, 0),
-    'location': '3D View Numerical Panel > Pose Tools',
-    'category': 'Animation',
+    "name": "Real pose copy",
+    "author": "Sybren A. Stüvel",
+    "version": (1, 1),
+    "blender": (2, 80, 0),
+    "location": "3D View Numerical Panel > Pose Tools",
+    "category": "Animation",
 }
 
 from collections import defaultdict, deque
@@ -25,14 +25,15 @@ from bpy.types import Menu, Panel, UIList
 
 
 class POSE_OT_copy_as_json(bpy.types.Operator):
-    bl_idname = 'pose.copy_as_json'
-    bl_label = 'Copy pose as JSON'
-    bl_description = 'Copies the matrices of the selected bones as JSON onto ' \
-        'the clipboard'
+    bl_idname = "pose.copy_as_json"
+    bl_label = "Copy pose as JSON"
+    bl_description = (
+        "Copies the matrices of the selected bones as JSON onto the clipboard"
+    )
 
     @classmethod
     def poll(cls, context):
-        return context.mode == 'POSE' and context.selected_pose_bones
+        return context.mode == "POSE" and context.selected_pose_bones
 
     def execute(self, context):
         context.scene.update()
@@ -40,22 +41,24 @@ class POSE_OT_copy_as_json(bpy.types.Operator):
         for bone in context.selected_pose_bones:
             # Convert matrix to list-of-tuples.
             vals = [tuple(v) for v in bone.matrix]
-            bone_data[bone.name]['matrix'] = vals
+            bone_data[bone.name]["matrix"] = vals
 
         context.window_manager.clipboard = json.dumps(bone_data)
-        self.report({'INFO'}, 'Selected pose bone matrices copied.')
+        self.report({"INFO"}, "Selected pose bone matrices copied.")
 
-        return {'FINISHED'}
+        return {"FINISHED"}
 
 
 class POSE_OT_paste_from_json(bpy.types.Operator):
-    bl_idname = 'pose.paste_from_json'
-    bl_label = 'Paste pose from JSON'
-    bl_description = 'Copies the matrices of the selected bones as JSON onto the clipboard'
+    bl_idname = "pose.paste_from_json"
+    bl_label = "Paste pose from JSON"
+    bl_description = (
+        "Copies the matrices of the selected bones as JSON onto the clipboard"
+    )
 
     @classmethod
     def poll(cls, context):
-        return context.mode == 'POSE'
+        return context.mode == "POSE"
 
     def execute(self, context):
         # Parse the JSON
@@ -63,17 +66,18 @@ class POSE_OT_paste_from_json(bpy.types.Operator):
         try:
             bone_data = json.loads(the_json)
         except ValueError as ex:
-            self.report({'ERROR'}, 'No valid JSON on clipboard: %s' % ex)
-            return {'CANCELLED'}
+            self.report({"ERROR"}, "No valid JSON on clipboard: %s" % ex)
+            return {"CANCELLED"}
 
         # Iterate over bones hierarchically, updating parents before children.
-        bones = deque(bone for bone in context.active_object.pose.bones
-                      if not bone.parent)
+        bones = deque(
+            bone for bone in context.active_object.pose.bones if not bone.parent
+        )
         while bones:
             bone = bones.popleft()
 
             try:
-                matrix_components = bone_data[bone.name]['matrix']
+                matrix_components = bone_data[bone.name]["matrix"]
             except KeyError:
                 pass  # This bone is not included in the pose JSON.
             else:
@@ -82,13 +86,13 @@ class POSE_OT_paste_from_json(bpy.types.Operator):
 
             bones.extend(bone.children)
 
-        self.report({'INFO'}, 'Pose bone matrices pasted.')
-        return {'FINISHED'}
+        self.report({"INFO"}, "Pose bone matrices pasted.")
+        return {"FINISHED"}
 
 
 class VIEW3D_PT_pose_tools(Panel):
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
     bl_category = "View"
     bl_label = "Copy Pose"
 
