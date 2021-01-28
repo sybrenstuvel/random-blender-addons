57e7c3c9fcbda3065decd8f3f9e8e34a8d23e19a
Sybren A. Stüvel <sybren@stuvel.eu>
2021-01-28 16:14:46 +0100

    Pose Lib Mockup: operator for creating Pose asset in current file

--- pose_library_mockup.py
+++ pose_library_mockup.py
@@ -19,10 +19,111 @@ from typing import Any, Dict, List, Optional, Set, Tuple, Union, cast
 
 import bpy
 from mathutils import Matrix
-from bpy.props import BoolProperty, EnumProperty
+from bpy.props import BoolProperty, EnumProperty, StringProperty, CollectionProperty
 from bpy.types import Action, Context, FCurve, Keyframe, Menu, Operator, Panel, UIList
 
 from bpy_extras import asset_utils
+
+
+def create_poselib_action(
+    context: Context,
+    src_action: Action,
+    src_frame_nr: float,
+    bone_names: Set[str],
+    pose_name: str,
+) -> Action:
+    """Create a single-frame Action containing only the given bones."""
+
+    dst_action = bpy.data.actions.new(pose_name)
+    dst_action.id_root = src_action.id_root
+
+    copy_fcurves(dst_action, src_action, src_frame_nr, bone_names)
+
+    asset_mark_ctx = {
+        **context.copy(),
+        "id": dst_action,
+    }
+    bpy.ops.asset.mark(asset_mark_ctx)
+    # TODO(Sybren): Add Asset metadata.
+
+    return dst_action
+
+
+def copy_fcurves(
+    dst_action: Action,
+    src_action: Action,
+    src_frame_nr: float,
+    bone_names: Set[str],
+) -> None:
+    pose_bone_re = re.compile(r'pose.bones\["([^"]+)"\]')
+    for fcurve in src_action.fcurves:
+        match = pose_bone_re.match(fcurve.data_path)
+        if not match:
+            continue
+
+        bone_name = match.group(1)
+        if bone_name not in bone_names:
+            continue
+
+        # Check if there is a keyframe on this frame.
+        keyframe = find_keyframe(fcurve, src_frame_nr)
+        if keyframe is None:
+            continue
+        # Create an FCurve and copy some properties.
+        src_group_name = fcurve.group.name if fcurve.group else ""
+        dst_fcurve = dst_action.fcurves.new(
+            fcurve.data_path, index=fcurve.array_index, action_group=src_group_name
+        )
+        for propname in {"auto_smoothing", "color", "color_mode", "extrapolation"}:
+            setattr(dst_fcurve, propname, getattr(fcurve, propname))
+
+        # Insert a single keyframe and copy some properties. The keyframe is
+        # eventually stored at frame=1 in the destination datablock. First
+        # it's placed at the original frame, though, so that the handles can
+        # be copied as-is. Later the `co_ui` attribute is used to move the
+        # keyframe and the handles in one go.
+        dst_keyframe = dst_fcurve.keyframe_points.insert(
+            keyframe.co.x, keyframe.co.y, keyframe_type=keyframe.type
+        )
+
+        for propname in {
+            "amplitude",
+            "back",
+            "easing",
+            "handle_left",
+            "handle_left_type",
+            "handle_right",
+            "handle_right_type",
+            "interpolation",
+            "period",
+        }:
+            setattr(dst_keyframe, propname, getattr(keyframe, propname))
+        dst_keyframe.co_ui.x = 1.0  # This also moves the handles.
+        dst_fcurve.update()
+
+
+def find_keyframe(fcurve: FCurve, frame: float) -> Optional[Keyframe]:
+    # Binary search adapted from https://pythonguides.com/python-binary-search/
+    keyframes = fcurve.keyframe_points
+    low = 0
+    high = len(keyframes) - 1
+    mid = 0
+
+    # Accept any keyframe that's within 'epsilon' of the requested frame.
+    # This should account for rounding errors and the likes.
+    epsilon = 1e-4
+    frame_lowerbound = frame - epsilon
+    frame_upperbound = frame + epsilon
+    while low <= high:
+        mid = (high + low) // 2
+        keyframe = keyframes[mid]
+        if keyframe.co.x < frame_lowerbound:
+            low = mid + 1
+        elif keyframe.co.x > frame_upperbound:
+            high = mid - 1
+        else:
+            return keyframe
+    return None
 
 
 class ANIM_OT_dummy(Operator):
@@ -34,11 +135,7 @@ class ANIM_OT_dummy(Operator):
         return {"CANCELLED"}
 
 
-class ANIM_OT_kick_to_own_file(Operator):
-    bl_idname = "anim.kick_to_own_file"
-    bl_label = "Store as one-frame Action"
-    bl_options = {"REGISTER"}
-
+class PoseAssetCreator:
     @classmethod
     def poll(cls, context: Context) -> bool:
         return bool(
@@ -53,99 +150,54 @@ class ANIM_OT_kick_to_own_file(Operator):
             and context.object.animation_data.action
         )
 
-    def execute(self, context: Context) -> Set[str]:
-        ob = context.object
 
+class ANIM_OT_create_pose_asset(PoseAssetCreator, Operator):
+    bl_idname = "anim.create_pose_asset"
+    bl_label = "Create Pose Asset"
+    bl_options = {"REGISTER", "UNDO"}
+
+    pose_name: StringProperty(name="Pose Name")  # type: ignore
+    # TODO(Sybren): allow setting tags here.
+
+    def execute(self, context: Context) -> Set[str]:
         src_action = context.object.animation_data.action
-        dst_action = bpy.data.actions.new("RENAME ME")
-        dst_action.id_root = src_action.id_root
 
         bones = context.selected_pose_bones_from_active_object
         bone_names = {bone.name for bone in bones}
 
-        self.copy_fcurves(
-            dst_action, src_action, bone_names, frame=context.scene.frame_current
+        create_poselib_action(
+            context, src_action, context.scene.frame_current, bone_names, self.pose_name
         )
-        self.add_asset_metadata(dst_action)
+        return {"FINISHED"}
+
+    def invoke(self, context: Context, _event: Any) -> Set[str]:
+        wm = context.window_manager
+        result = wm.invoke_props_dialog(self)
+        return cast(Set[str], result)
+
+
+class ANIM_OT_kick_to_own_file(PoseAssetCreator, Operator):
+    bl_idname = "anim.kick_to_own_file"
+    bl_label = "Store as one-frame Action"
+    bl_options = {"REGISTER"}
+
+    def execute(self, context: Context) -> Set[str]:
+        src_action = context.object.animation_data.action
+
+        bones = context.selected_pose_bones_from_active_object
+        bone_names = {bone.name for bone in bones}
+
+        dst_action = create_poselib_action(
+            context,
+            src_action,
+            context.scene.frame_current,
+            bone_names,
+            "RENAME ME",
+        )
+
         self.save_datablock(dst_action)
         bpy.data.actions.remove(dst_action)
         return {"FINISHED"}
-
-    def copy_fcurves(
-        self, dst_action: Action, src_action: Action, bone_names: Set[str], frame: float
-    ) -> None:
-        pose_bone_re = re.compile(r'pose.bones\["([^"]+)"\]')
-        for fcurve in src_action.fcurves:
-            match = pose_bone_re.match(fcurve.data_path)
-            if not match:
-                continue
-
-            bone_name = match.group(1)
-            if bone_name not in bone_names:
-                continue
-
-            # Check if there is a keyframe on this frame.
-            keyframe = self.find_keyframe(fcurve, frame)
-            if keyframe is None:
-                continue
-            # Create an FCurve and copy some properties.
-            src_group_name = fcurve.group.name if fcurve.group else ""
-            dst_fcurve = dst_action.fcurves.new(
-                fcurve.data_path, index=fcurve.array_index, action_group=src_group_name
-            )
-            for propname in {"auto_smoothing", "color", "color_mode", "extrapolation"}:
-                setattr(dst_fcurve, propname, getattr(fcurve, propname))
-
-            # Insert a single keyframe and copy some properties. The keyframe is
-            # eventually stored at frame=1 in the destination datablock. First
-            # it's placed at the original frame, though, so that the handles can
-            # be copied as-is. Later the `co_ui` attribute is used to move the
-            # keyframe and the handles in one go.
-            dst_keyframe = dst_fcurve.keyframe_points.insert(
-                keyframe.co.x, keyframe.co.y, keyframe_type=keyframe.type
-            )
-
-            for propname in {
-                "amplitude",
-                "back",
-                "easing",
-                "handle_left",
-                "handle_left_type",
-                "handle_right",
-                "handle_right_type",
-                "interpolation",
-                "period",
-            }:
-                setattr(dst_keyframe, propname, getattr(keyframe, propname))
-            dst_keyframe.co_ui.x = 1.0  # This also moves the handles.
-            dst_fcurve.update()
-
-    def find_keyframe(self, fcurve: FCurve, frame: float) -> Optional[Keyframe]:
-        # Binary search adapted from https://pythonguides.com/python-binary-search/
-        keyframes = fcurve.keyframe_points
-        low = 0
-        high = len(keyframes) - 1
-        mid = 0
-
-        # Accept any keyframe that's within 'epsilon' of the requested frame.
-        # This should account for rounding errors and the likes.
-        epsilon = 1e-4
-        frame_lowerbound = frame - epsilon
-        frame_upperbound = frame + epsilon
-        while low <= high:
-            mid = (high + low) // 2
-            keyframe = keyframes[mid]
-            if keyframe.co.x < frame_lowerbound:
-                low = mid + 1
-            elif keyframe.co.x > frame_upperbound:
-                high = mid - 1
-            else:
-                return keyframe
-        return None
-
-    def add_asset_metadata(self, action: Action) -> None:
-        # TODO: implement
-        pass
 
     def save_datablock(self, action: Action) -> None:
         bpy.data.libraries.write(
@@ -189,12 +241,23 @@ class ASSETBROWSER_PT_pose_library_npanel(asset_utils.AssetBrowserPanel, Panel):
         # col.label(text="Application")
         col.prop(context.window_manager, "poselib_apply_flipped")
         row = col.row(align=True)
-        row.prop_enum(context.window_manager, "poselib_merge_choices", "INSERT")
+        if False:  # if selected pose is animation
+            row.prop_enum(context.window_manager, "poselib_merge_choices", "INSERT")
         row.prop_enum(context.window_manager, "poselib_merge_choices", "REPLACE")
         row.prop_enum(context.window_manager, "poselib_merge_choices", "BLEND")
 
         row = col.row(align=True)
         row.operator("anim.dummy", text="Apply Pose")
+
+
+class DOPESHEET_PT_asset_panel(Panel):
+    bl_space_type = "DOPESHEET_EDITOR"
+    bl_region_type = "UI"
+    bl_label = "Pose Library"
+
+    def draw(self, context: Context) -> None:
+        layout = self.layout
+        layout.operator("anim.create_pose_asset")
 
 
 class ASSETBROWSER_PT_pose_library_tools(asset_utils.AssetBrowserPanel, Panel):
@@ -205,7 +268,7 @@ class ASSETBROWSER_PT_pose_library_tools(asset_utils.AssetBrowserPanel, Panel):
         layout = self.layout
 
         col = layout.grid_flow(row_major=True, align=True, columns=2)
-        col.operator("anim.dummy", text="Approved", depress=True)
+        col.operator("anim.dummy", text="★", depress=True)
         col.operator("anim.dummy", text="Front")
         col.operator("anim.dummy", text="Happy")
         col.operator("anim.dummy", text="Side")
@@ -225,10 +288,12 @@ class ASSETBROWSER_PT_pose_library_tools(asset_utils.AssetBrowserPanel, Panel):
 
 
 classes = (
+    ANIM_OT_create_pose_asset,
     ANIM_OT_dummy,
     ANIM_OT_kick_to_own_file,
     ASSETBROWSER_PT_pose_library_npanel,
     ASSETBROWSER_PT_pose_library_tools,
+    DOPESHEET_PT_asset_panel,
     VIEW3D_PT_pose_library,
 )
 
