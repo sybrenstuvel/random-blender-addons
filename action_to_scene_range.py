ee9aca851e06cf596273144239b7abce38da97e4
Sybren A. Stüvel <sybren@blender.org>
2024-05-02 10:53:28 +0200

    Disable the operator when the scene is already at the right range

--- action_to_scene_range.py
+++ action_to_scene_range.py
@@ -12,7 +12,7 @@ its length.
 bl_info = {
     "name": "Action to Scene Range",
     "author": "Sybren A. Stüvel",
-    "version": (1, 1),
+    "version": (1, 2),
     "blender": (4, 1, 0),
     "location": "Automatic, no interface available",
     "category": "Animation",
@@ -33,13 +33,24 @@ class ACTION_OT_to_scene_range(bpy.types.Operator):
 
     @classmethod
     def poll(cls, context: bpy.types.Context) -> bool:
-        return context.space_data.mode == 'ACTION' and bool(context.space_data.action)
+        if context.space_data.mode != 'ACTION':
+            return False
+
+        action = context.space_data.action
+        scene = context.scene
+
+        if not action or not scene:
+            return False
+        if _get_range_action(action) == _get_range_scene(scene):
+            cls.poll_message_set("Scene already at Action range")
+            return False
+        return True
 
     def execute(self, context: bpy.types.Context) -> set[str]:
         action = context.space_data.action
         scene = context.scene
-        start, end = _action_to_scene_range(action, scene)
 
+        start, end = _action_to_scene_range(action, scene)
         self.report({'INFO'}, f"Changed scene range to {start}-{end}")
 
         return {'FINISHED'}
@@ -49,10 +60,21 @@ classes = (ACTION_OT_to_scene_range,)
 _register, _unregister = bpy.utils.register_classes_factory(classes)
 
 
-def _action_to_scene_range(action: bpy.types.Action, scene: bpy.types.Scene) -> tuple[float, float]:
+def _get_range_action(action: bpy.types.Action) -> tuple[int, int]:
     action_range = tuple(action.frame_range)
     frame_start = int(action_range[0])
     frame_end = int(action_range[1])
+    return frame_start, frame_end
+
+
+def _get_range_scene(scene: bpy.types.Scene) -> tuple[int, int]:
+    if scene.use_preview_range:
+        return scene.frame_preview_start, scene.frame_preview_end
+    return scene.frame_start, scene.frame_end
+
+
+def _action_to_scene_range(action: bpy.types.Action, scene: bpy.types.Scene) -> tuple[float, float]:
+    frame_start, frame_end = _get_range_action(action)
 
     if scene.use_preview_range:
         scene.frame_preview_start = frame_start
