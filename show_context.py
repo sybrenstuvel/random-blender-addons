78059a398f25f12246496fc3543292a708d3278a
Sybren A. Stüvel <sybren@blender.org>
2024-05-02 10:44:01 +0200

    Add operator to show space data properties

--- show_context.py
+++ show_context.py
@@ -1,7 +1,7 @@
 bl_info = {
     'name': 'Show Context',
     'author': 'Sybren A. Stüvel',
-    'version': (1, 0),
+    'version': (1, 1),
     'blender': (4, 1, 0),
     'location': 'Anywhere',
     'category': 'Developer',
@@ -19,13 +19,32 @@ class DEVELOPER_OT_show_context(bpy.types.Operator):
     bl_options = {'REGISTER'}
 
     def execute(self, context: bpy.types.Context) -> set[str]:
-        to_print = {name: repr(getattr(context, name, '-error-')) for name in dir(context)}
-
-        print("--- Context: " + 30 * "-")
-        pprint(to_print, width=120)
+        _show_properties("Context", context)
         self.report({'INFO'}, "Check the terminal")
         return {'FINISHED'}
 
 
-classes = (DEVELOPER_OT_show_context,)
+class DEVELOPER_OT_show_space_data(bpy.types.Operator):
+    bl_idname = 'developer.show_space_data'
+    bl_label = 'Show Space Data'
+    bl_description = 'List the space data properties in the terminal'
+    bl_options = {'REGISTER'}
+
+    def execute(self, context: bpy.types.Context) -> set[str]:
+        _show_properties("Space Data", context.space_data)
+        self.report({'INFO'}, "Check the terminal")
+        return {'FINISHED'}
+
+
+def _show_properties(header: str, python_object: object) -> None:
+    to_print = {name: repr(getattr(python_object, name, '-error-')) for name in dir(python_object)}
+
+    print(f"--- {header}: {30*'-'}")
+    pprint(to_print, width=120)
+
+
+classes = (
+    DEVELOPER_OT_show_context,
+    DEVELOPER_OT_show_space_data,
+)
 register, unregister = bpy.utils.register_classes_factory(classes)
